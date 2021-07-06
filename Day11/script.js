a0a7a069ea7af77bd9a26e36a5ace8a92b8a8228
utkarshchauhan007 <61346045+utkarshchauhan007@users.noreply.github.com>
2021-07-06 18:13:16 +0530

    day11

---
+++ Day11/script.js
@@ -0,0 +1,28 @@
+const quiz = [
+    {
+        q:'Which month comes right before june ?',
+        options:['may','september','july','august'],
+        answer:0
+    },
+    {
+        q:'What color is banana ? ',
+        options:['red','yellow','white','blue'],
+        answer:1
+    },
+    {
+        q:'3 + 4 = 7 ?',
+        options:['true','false'],
+        answer:1
+    },
+    {
+       q:'when do you have breakfast ? ',
+       options:['afternoon','morning','evening'],
+       answer:1
+    },
+    {
+        q:'what is 22 + 6 = ?',
+      options:['99','56','16','28'],
+      answer:3
+  
+    }
+  ]
